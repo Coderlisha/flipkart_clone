29b84b4edcfa1a16585d8deb562d3252deec1f8e
Lisha <marathelisha@gmail.com>
2025-02-20 23:20:20 +0530

    first commit

--- server/database/db.js
+++ server/database/db.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 // const password = "satyam123";
 
 const Connection = async () => {
-    const URL = `mongodb://localhost:27017/e-com`;
+    const URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.qzc5c.mongodb.net/shopify?retryWrites=true&w=majority&appName=Cluster0`;
     
     try {
         await mongoose.connect(URL, {
